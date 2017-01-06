aedba6f374a594f25ac0aad734a601bdce47d3ea
Arthur Dingemans <arthur@argu.co>
2017-01-06 10:32:33 +0100

    [IMP] Update service_base [IMP] Overwrite 404 error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ServiceBase::ApiController
   def client_token
     request.cookie_jar.encrypted['client_token']
   end
+
+  def handle_record_not_found_error
+    respond_to do |format|
+      format.html do
+        send_file File.expand_path('../../../public/404.html', __FILE__), disposition: :inline, status: 404
+      end
+      format.json { render json_api_error(404, 'Please provide a valid token') }
+    end
+  end
 end
