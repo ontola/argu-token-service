1728fd39fa7638e5c23e4161d20a251c10cab342
Arthur Dingemans <arthur@argu.co>
2017-11-03 13:31:31 +0100

    [TS-41] Don’t include used email tokens in Token#index

--- app/controllers/email_token_controller.rb
+++ app/controllers/email_token_controller.rb
@@ -2,7 +2,7 @@
 
 class EmailTokenController < TokensController
   def index
-    render json: Token.email.active.where(group_id: group_id), include: {emails: :email_events}
+    render json: Token.email.active.where(last_used_at: nil, group_id: group_id), include: {emails: :email_events}
   end
 
   private
