c76b4ec0db3ca4617290046a5e2d178ffc5776f5
Arthur Dingemans <arthur@argu.co>
2019-04-08 12:23:51 +0200

    [TS][AOD-469] Update website meta header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,20 @@ class ApplicationController < ApiController
   end
 
   def set_tenant_header
-    response.headers['Tenant-IRI'] = "https://#{tree_root.iri_prefix}" if tree_root
+    response.headers['Website-Meta'] = website_meta.to_query if tree_root
   end
 
   def tree_root
     ActsAsTenant.current_tenant
+  end
+
+  def website_meta
+    {
+      accent_background_color: tree_root.accent_background_color,
+      accent_color: tree_root.accent_color,
+      iri: "https://#{tree_root.iri_prefix}",
+      navbar_background: tree_root.navbar_background,
+      navbar_color: tree_root.navbar_color
+    }
   end
 end
