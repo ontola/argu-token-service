17033098ba71925b060fff67089c7e5799b589dd
Arthur Dingemans <arthur@argu.co>
2020-08-24 08:42:08 +0200

    core#12 Fix redirecting urls with old iri prefixes

---
+++ spec/requests/head_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Head' do
+  let(:token) { create(:token) }
+
+  before do
+    as_guest
+  end
+
+  it 'handles a valid request' do
+    head "/argu/tokens/#{token.secret}", headers: service_headers(accept: :nq)
+    assert_response 200
+  end
+
+  it 'handles a wrong iri prefix' do
+    find_tenant_mock("#{ENV['HOSTNAME']}\/wrong\/tokens.*", shortnames: %w[argu wrong])
+    Rails.application.config.origin = Rails.application.config.origin.sub('https', 'http')
+    head "/wrong/tokens/#{token.secret}", headers: service_headers(accept: :nq)
+    assert_redirected_to resource_iri(token).to_s
+    Rails.application.config.origin = Rails.application.config.origin.sub('http', 'https')
+  end
+end
