c9f2aed95baac1074c6f177096284e6a2de2b9c4
Arthur Dingemans <arthur@argu.co>
2022-09-01 15:20:50 +0200

    fix: don't allow updating email fields after send

--- app/policies/email_token_policy.rb
+++ app/policies/email_token_policy.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class EmailTokenPolicy < TokenPolicy
-  permit_attributes %i[actor_iri message redirect_url root_id send_mail]
-  permit_array_attributes %i[addresses]
+  permit_attributes %i[redirect_url]
+  permit_attributes %i[actor_iri message root_id send_mail], new_record: true
+  permit_array_attributes %i[addresses], new_record: true
 end
